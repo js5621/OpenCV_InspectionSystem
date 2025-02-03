88f7a774f73706a79c10d50c9b4f2989809785bc
JongSeong YUN <asaph5621@gmail.com>
2025-02-04 01:28:12 +0900

    OpenCV First Test

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using OpenCvSharp;
+
+namespace OpenCVProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 사진 출력
+             Mat image = Cv2.ImRead("BeastEdit.png", ImreadModes.Grayscale);
+             Cv2.ImShow("image", image);
+             Cv2.WaitKey(0);
+             Cv2.DestroyAllWindows();
+             */
+            /* 웹캠출력
+            VideoCapture video = new VideoCapture(0);
+            Mat frame = new Mat();
+
+            while (Cv2.WaitKey(33) != 'q')
+            {
+                video.Read(frame);
+                Cv2.ImShow("frame", frame);
+            }
+
+            frame.Dispose();
+            video.Release();
+            Cv2.DestroyAllWindows();
+            */
+            // 이미지 크기조정
+            Mat src = new Mat("BeastEdit.png");
+            Mat dst = new Mat();
+
+            Cv2.Resize(src, dst, new Size(50, 50));
+
+            Cv2.ImShow("src", src);
+            Cv2.ImShow("dst", dst);
+            Cv2.WaitKey(0);
+        }
+    }
+}
